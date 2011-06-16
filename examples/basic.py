cf6f35f016c844fed94e927362622dad92771ce2
Alice Bevan-McGregor <alice@gothcandy.com>
2011-06-15 23:21:46 -0400

    More complete example with documents, fields, etc.  In this case it is the generic authentication model for `marrow.auth`.

--- examples/basic.py
+++ examples/basic.py
@@ -2,9 +2,64 @@
 
 from __future__ import unicode_literals
 
+import string
+import scrypt
+
+from datetime import datetime
+from random import choice
+
 from marrow.norm.connection import Connection
+from marrow.norm.document import Document
+from marrow.norm.field import *
 
 
+
+class Credential(Document):
+    def validate(self):
+        raise NotImplementedError
+
+
+class PasswordCredential(Credential):
+    __password = String(key='p')
+    difficulty = Float(key='d', default=0.5) # seconds
+    
+    @property
+    def password(self):
+        return None
+    
+    @password.setter
+    def password(self, value):
+        salt = ''.join([choice(string.printable) for i in range(64)])
+        self.__password = scrypt.encrypt(salt, value, maxtime=self.difficulty)
+    
+    @password.deleter
+    def password(self):
+        self.__password = None
+    
+    def validate(self, password):
+        try:
+            scrypt.decrypt(self.__password, password, self.difficulty)
+        
+        except scrypt.error:
+            return False
+        
+        return True
+
+
+class FailedLogin(Document):
+    location = IPAddress(key='ip')
+    when = DateTime(key='ts', default=lambda: datetime.utcnow())
+
+
+class User(Document):
+    identity = String(required=True)
+    credentials = List(Embed, Credential)
+    
+    created = DateTime()
+    modified = DateTime()
+    
+    last = DateTime()
+    failed = List(Embed, FailedLogin)
 
 
 if __name__ == '__main__':
@@ -13,4 +68,3 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     
     conn = Connection("mongodb://localhost/test")
-    
